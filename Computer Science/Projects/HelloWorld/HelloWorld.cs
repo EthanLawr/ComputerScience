ee950bb548740f65e1641ff9aca10aa1c6fb6f43
Ethan <elawrence152@gmail.com>
2017-09-26 11:41:53 -0400

    Add files via upload

---
+++ Computer Science/Projects/HelloWorld/HelloWorld.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+namespace HelloWorld
+{
+    class HelloWorld
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("\"hello\" kek"); //Header
+            Console.WriteLine("Press ENTER"); //Footer
+            Console.Read(); //Lets the Command Console Stay
+        }
+    }
+};
