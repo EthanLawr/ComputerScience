4b0113d234949cfe3f04a75f4c35ddaf91f7b2f7
Ethan <elawrence152@gmail.com>
2017-09-26 10:47:42 -0400

    Add files via upload

---
+++ Computer Science/Projects/Class1.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Ling;
+using System.Text;
+namespace HelloWorld
+{
+    class HelloWorld
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("\"hello\" kek"); //welp
+            Console.Read();
+        }
+    }
+};
