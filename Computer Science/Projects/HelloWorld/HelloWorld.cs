fd7b270750717d116b2600688787bb856bb1b8bc
Ethan <elawrence152@gmail.com>
2017-09-28 08:37:35 -0400

    CoLoRs

--- Computer Science/Projects/HelloWorld/HelloWorld.cs
+++ Computer Science/Projects/HelloWorld/HelloWorld.cs
@@ -8,8 +8,12 @@ namespace HelloWorld
     {
         static void Main(string[] args)
         {
+            Console.BackgroundColor = ConsoleColor.Magenta; //Color Set To Magenta
             Console.WriteLine("\"hello\" kek"); //Header
+            Console.ResetColor(); //Color Reset
+            Console.BackgroundColor = ConsoleColor.Red; //Color Set To Res
             Console.WriteLine("Press ENTER"); //Footer
+            Console.ResetColor(); //Color Reset
             Console.Read(); //Lets the Command Console Stay
         }
     }
