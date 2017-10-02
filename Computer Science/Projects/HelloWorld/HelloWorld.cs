dbd208658f5bef68b66205411e89ca825e0a573f
Ethan <elawrence152@gmail.com>
2017-10-02 11:21:30 -0400

    Add files via upload

--- Computer Science/Projects/HelloWorld/HelloWorld.cs
+++ Computer Science/Projects/HelloWorld/HelloWorld.cs
@@ -2,25 +2,66 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
+
 namespace HelloWorld
 {
     class HelloWorld
     {
         static void Main(string[] args)
         {
-            Console.ForegroundColor = ConsoleColor.Cyan; //Color Set to Red
-            Console.WriteLine("Ethan Lawrence"
-                + "\t" + DateTime.Now
-                + "\tPeriod 4,5,6"
-                + "\tMr. Lynch"
-                + "\n********************************************************************************");
-            Console.ResetColor(); //Color Reset of the text
-            Console.ForegroundColor = ConsoleColor.Magenta; //Color Set to Magenta
-            Console.WriteLine("\"Hello\" World"); //Header
-            Console.ResetColor(); //Color Reset
-            Console.ForegroundColor = ConsoleColor.Cyan; //Color Set to Red
+            #region Header
+            Console.ForegroundColor = ConsoleColor.Cyan; //Sets the color to Cyan
+            string name = "Ethan Lawrence";
+            //Allows me to type my name in a shorter amount of time
+            var date = "\t" + System.IO.File.GetLastWriteTime("Path.GetFileName(fileName)");
+            //Gives the date and time of when the file was last saved
+            string period = "\tPeriod 4,5,6";
+            //Shows what periods the class is from
+            string teacher = "\tMr. Lynch";
+            //Shows my teacher's last name
+            string purpose = "\nPurpose: To make a Hello World Program";
+            //Shows the purpose of the current project
+            string endheader = "\n********************************************************************************";
+            //Ends the header
+            Console.WriteLine(name); //Writes my name
+            Thread.Sleep(50); //Sustains my name
+            Console.Clear(); //Clears my name
+            Thread.Sleep(70); //"Loading" time
+            Console.WriteLine(name + date);
+            //Writes my name and the date w/ the time
+            Thread.Sleep(70); //Sustains my name and the date w/ the time
+            Console.Clear(); //Clears my name and the date w/ the time
+            Thread.Sleep(90); //"Loading" time
+            Console.WriteLine(name + date + period);
+            //Writes my name, the date w/ the time, and the period
+            Thread.Sleep(90); //Sustains my name, the date w/ the time, and the period
+            Console.Clear(); //Clears my name, the date w/ the time, and the period
+            Thread.Sleep(110); //"Loading" Time
+            Console.WriteLine(name + date + period + teacher);
+            //Writes my name, the date w/ the time, period, and teacher name
+            Thread.Sleep(110); //Sustains my name, the date w/ the time, period, and teacher name
+            Console.Clear(); //Clears my name, the date w/ the time, period, and teacher name
+            Thread.Sleep(130);
+            Console.WriteLine(name + date + period + teacher + purpose);
+            //Writes my name, the date w/ the time, period, teacher name, and purpose
+            Thread.Sleep(130); //Sustains my name, the date w/ the time, period, teacher name, and purpose
+            Console.Clear(); //Clears my name, the date w/ the time, period, teacher name, and purpose
+            Thread.Sleep(150); //"Loading" Time
+            Console.WriteLine(name + date + period + teacher + purpose + endheader);
+            //Writes my name, the date w/ the time, period, teacher name, purpose, and ends the header
+            Thread.Sleep(150); //"Loading" Time
+            Console.ResetColor(); //Resets the color for non header lines
+            #endregion Header
+
+            Console.WriteLine("\"Hello\" World");
+
+            #region Footer
+            Console.ForegroundColor = ConsoleColor.Cyan; //Color Set to Cyan
             Console.WriteLine("\n\n\nPress any key to continue..."); //Footer
             Console.ResetColor(); //Color Reset of the text
+            #endregion Footer
+
             Console.ReadKey(); //Lets the Command Console stay until a key is pressed 
         }
     }
