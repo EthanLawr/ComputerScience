e43c6dcf3187af9119c02ab8ce9dc8a59346a699
Ethan <elawrence152@gmail.com>
2017-10-06 11:37:03 -0400

    moar

---
+++ Administrator Comp Sci 1/2016-2017/Day1_Per3_RefAndOutPractice_Solution/Day1_Per3_RefAndOutPractice/Day1_Per3_RefAndOutPractice.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day1_Per3_RefAndOutPractice
+{
+    class Day1_Per3_RefAndOutPractice
+    {
+        public static void Header()
+        {
+             #region Header
+            Console.SetWindowSize(80, 60);
+            Console.ForegroundColor = ConsoleColor.Cyan;
+            Console.WriteLine("Title: <insert here>\t\tAuthor: Lynch");
+            Console.WriteLine("\nDate: <insert here>");
+            Console.WriteLine("\nPurpose: <insert here>");
+            Console.WriteLine("\n****************************************\n\n\n");
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            #endregion
+
+        }
+        static void Main(string[] args)
+        {
+            Header();
+
+            //Working with ref and out
+            string nickName = "Dude";//need "ref" because initialized
+            string gatoradeFlavor;//need "out" because just declared, not initialized
+            Console.WriteLine("initially nickname is {0}",nickName);
+
+            double gPA = AskForDouble("GPA",ref nickName, out gatoradeFlavor);
+            Console.WriteLine("you typed GPA = {0}",gPA);
+            Console.WriteLine("After using 'ref' your nickname is {0}",nickName);
+            Console.WriteLine("After using 'out' your favorite flavor = {0}",gatoradeFlavor);
+
+
+            EndProgram();
+        }
+        public static double AskForDouble(string z, ref string nName, out string gFlav)
+        {
+            Console.WriteLine("Please enter your {0}",z);
+            nName = "Knucklehead";
+            string input = Console.ReadLine();
+            double inputDouble = Convert.ToDouble(input);
+            Console.WriteLine("Enter favorite gatorade flavor");
+            gFlav = Console.ReadLine();
+            //gFlav = "cherry";
+            return inputDouble;
+
+        }
+        public static void EndProgram()
+        {
+            #region This is where we end the program
+            //End any program
+            Console.BackgroundColor = ConsoleColor.White;
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.Write("\n\n\nPlease hit the ");
+            Console.BackgroundColor = ConsoleColor.DarkMagenta;
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.Write(" ENTER ");
+            Console.BackgroundColor = ConsoleColor.White;
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine(" key to end program . . . ");
+            Console.Read();
+            #endregion
+        }
+    }
+}
---
+++ Administrator Comp Sci 1/2016-2017/Day1_Per3_StepInto_Solution/Day1_Per3_StepInto/Day1_Per3_StepInto.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day1_Per3_StepInto
+{
+    class Day1_Per3_StepInto
+    {
+        static void Main(string[] args)
+        { 
+            #region Header
+            Console.SetWindowSize(80, 60);
+            Console.ForegroundColor = ConsoleColor.Cyan;
+            Console.WriteLine("Title: <insert here>\t\tAuthor: Lynch");
+            Console.WriteLine("\nDate: <insert here>");
+            Console.WriteLine("\nPurpose: <insert here>");
+            Console.WriteLine("\n****************************************\n\n\n");
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            #endregion
+
+            //Step Into for math heavy calculations
+
+            int radiusSphere = 7;
+            double coefficient = (double)4 / 3; //Cast
+            const double PI = 3.1415926;
+            // Formula (4/3) * PI * radius^3
+            Console.WriteLine(Math.PI); 
+            double volumeSphere = coefficient * PI * 
+                radiusSphere * radiusSphere * radiusSphere;
+
+            Console.WriteLine("If the radius is {0} inches, then the volume is "
+                + "{1:N3} inches cubed",radiusSphere,volumeSphere);
+
+            #region This is where we end the program
+            //End any program
+            Console.BackgroundColor = ConsoleColor.White;
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.Write("\n\n\nPlease hit the ");
+            Console.BackgroundColor = ConsoleColor.DarkMagenta;
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.Write(" ENTER ");
+            Console.BackgroundColor = ConsoleColor.White;
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine(" key to end program . . . ");
+            Console.Read();
+            #endregion
+        }
+    }
+}
