25fd0c7a89a84ed7a05613ffd30671d5ef7b715f
Ethan <elawrence152@gmail.com>
2017-10-06 11:42:44 -0400

    buncha sht

---
+++ Administrator Comp Sci 1/2016-2017/Day2_Per78_Chapter4_Practice_Solution/Day2_Per78_Chapter4_Practice/Day2_Per78_Chapter4_Practice.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day2_Per78_Chapter4_Practice
+{
+    class Day2_Per78_Chapter4_Practice
+    {
+        public static void Header()
+        {
+            Console.SetWindowSize(80, 60);
+            Console.ForegroundColor = ConsoleColor.Cyan;
+            Console.WriteLine("\nTitle: <insert here>\t\tAuthor: Lynch");
+            Console.WriteLine("\nDate: <insert here>");
+            Console.WriteLine("\nPurpose: <insert here>");
+            Console.WriteLine("\n****************************************\n\n\n");
+            Console.ForegroundColor = ConsoleColor.Yellow;
+        }
+
+        public static void HitAnyKeyToContinue()
+        {
+            Console.Write("\n\nPlease hit any key to continue . . . \t\t");
+            Console.ReadKey();
+        }
+
+        static void Main(string[] args)//private is assumed
+        {
+            HitAnyKeyToContinue();
+            HitAnyKeyToContinue();
+            HitAnyKeyToContinue();
+
+            //Integer inputs
+            int integerA = AskUserForInt("A");
+            int integerB = AskUserForInt("B");
+            int integerC = AskUserForInt("C");
+
+            //Let's do a method where we send 2 things into it
+
+            RootsOfTheQuadraticEquation(integerA, integerB, integerC);
+            Console.WriteLine("");
+
+            #region Old Stuff
+            //Void method
+            Day2_Per78_Chapter4_Practice.Header();
+            Header();
+
+            //declare a bunch of strings
+            string momName, dadName, favSubject, dogBreed;
+
+            momName = AskUserForString("your mom's name");
+            dadName = AskUserForString("your dad's name");
+            favSubject = AskUserForString("your favorite subject");
+
+            Console.WriteLine("Your mom is {0} and your"
+                + " dad is {1}",momName,dadName);
+
+            //double returning values
+            double weightInTenths = AskUserForDouble("your weight");
+            double heightInTenths = AskUserForDouble("your height");
+
+            
+            Multiply(weightInTenths, heightInTenths);
+            //need to call the method from the Main(); 
+
+            EndProgram();
+#endregion
+        }
+        public static void RootsOfTheQuadraticEquation(int a, int b, int c)
+        {
+            //void method that needs 3 outside variables sent to it!
+            double root1 = (-b + Math.Sqrt(Math.Pow(b, 2) - 4 * a * c)) / (2 * a);
+            double root2 = (-b - Math.Sqrt(Math.Pow(b, 2) - 4 * a * c)) / (2 * a);
+
+            Console.WriteLine("if A = {0}",a);
+            Console.WriteLine("if B = {0}", b);
+            Console.WriteLine("if C = {0}", c);
+            Console.WriteLine("then roots are {0} and {1}",root1,root2);
+            //no return statement needed because void
+        }
+
+        public static int AskUserForInt(string x)//string x is just a placeholder
+        {                                       //for the string in the Main{}
+            Console.WriteLine("\n\nEnter the {0} integer",x);
+            string inputString = Console.ReadLine();//always stores string
+            int inputInt = Convert.ToInt32(inputString);//changes string to int
+            return inputInt;//need a return statement to send back
+            //to Main method
+        }
+
+        public static void Multiply(double mainWeight, double mainHeight)
+        {
+            double product = mainWeight * mainHeight;
+            Console.WriteLine("your weight * height is = {0}",product);
+        }
+
+        public static double AskUserForDouble(string mainString)
+        {
+            Console.WriteLine("Please enter . . . {0}",mainString);
+            string inputString = Console.ReadLine();
+            double inputDouble = Convert.ToDouble(inputString);
+            return inputDouble;
+
+        }
+
+        public static string AskUserForString(string mainString)
+        {//return a value string
+            Console.WriteLine("Please enter . . . {0}",mainString);
+            string inputString = Console.ReadLine();//stores a string
+            return inputString;//returns the string back to Main();
+        }
+
+        public static void EndProgram()
+        {
+            //#region This is where we end the program
+            //No need for regions inside methods
+
+            //End any program
+            Console.BackgroundColor = ConsoleColor.White;
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.Write("\n\n\nPlease hit the ");
+            Console.BackgroundColor = ConsoleColor.DarkMagenta;
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.Write(" ENTER ");
+            Console.BackgroundColor = ConsoleColor.White;
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine(" key to end program . . . ");
+            Console.Read();
+            //#endregion
+        }
+    }
+}
---
+++ Administrator Comp Sci 1/2016-2017/Day2_Per78_RefAndOutPractice_Solution/Day2_Per78_RefAndOutPractice/Day2_Per78_RefAndOutPractice.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day2_Per78_RefAndOutPractice
+{
+    class Day2_Per78_RefAndOutPractice
+    {
+        public static void Header()
+        {
+            #region Header
+            Console.SetWindowSize(80, 60);
+            Console.ForegroundColor = ConsoleColor.Cyan;
+            Console.WriteLine("Title: <insert here>\t\tAuthor: Lynch");
+            Console.WriteLine("\nDate: <insert here>");
+            Console.WriteLine("\nPurpose: <insert here>");
+            Console.WriteLine("\n****************************************\n\n\n");
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            #endregion
+        }
+        static void Main(string[] args)
+        {
+            Header();
+
+            //Old stuff
+            string country;//just declared as a string (use out parameter)
+            double gPA = 3.6;//declared as double and initialized (use ref parameter)
+            Console.WriteLine("original gpa is {0}",gPA);
+            
+            // using 'ref' and 'out' parameters
+            string worstStudent = AskUserForString("worst student", out country, ref gPA);
+            //allows us to return or change three different variables in same method!!!
+            //'worstStudent' and 'country' and 'gPA' will all be changed or given values
+            //from the AskUserForString() method!
+
+            Display(worstStudent, country, gPA);
+
+            EndProgram();
+        }
+        public static void Display(string z, string x, double y)
+        {
+            Console.WriteLine("Original worst student = {0}",z);
+            Console.WriteLine("favorite country = {0}",x);
+            Console.WriteLine("your GPA is {0}",y);
+        }
+
+        public static string AskUserForString(string a, out string b, ref double c)
+        {
+            Console.WriteLine("Please enter your {0} name",a);
+            string input = Console.ReadLine();
+            b = "USA";//'b' is just a placeholder for 'country' in Main()
+            //and uses 'out' parameter
+            Console.WriteLine("Hack in the GPA you want");
+            c = Convert.ToDouble(Console.ReadLine());//converts string to double in one step
+            //'c' is a placeholder for 'gPA' and uses 'ref' parameter because 
+            //'gPA' was already initialized in Main()
+            return input;//returns the first input back to Main()
+        }
+        public static void EndProgram()
+        {
+            #region This is where we end the program
+            //End any program
+            Console.BackgroundColor = ConsoleColor.White;
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.Write("\n\n\nPlease hit the ");
+            Console.BackgroundColor = ConsoleColor.DarkMagenta;
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.Write(" ENTER ");
+            Console.BackgroundColor = ConsoleColor.White;
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine(" key to end program . . . ");
+            Console.Read();
+            #endregion
+        }
+    }
+}
