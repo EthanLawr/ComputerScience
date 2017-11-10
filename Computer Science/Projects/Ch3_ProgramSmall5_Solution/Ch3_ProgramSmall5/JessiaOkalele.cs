9346376898d46b4984156084167537b8721e84fc
Ethan <elawrence152@gmail.com>
2017-11-10 11:32:11 -0500

    Add files via upload

---
+++ Computer Science/Projects/Ch3_ProgramSmall5_Solution/Ch3_ProgramSmall5/JessiaOkalele.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ch3_ProgramSmall5
+{
+    class JessiaOkalele
+    {
+        static void Main(string[] args)
+        {
+         /* string employeeName = "Jessica Oakley";  
+         28000$ sales
+                10% retire
+                18% fed tax
+                6% social security
+                gets 7% of her sales */
+        }
+    }
+}
