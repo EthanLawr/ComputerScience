b8379b1b55888d76ad480e27b2ef70c75656b5b7
Ethan <elawrence152@gmail.com>
2018-01-02 11:42:56 -0500

    Add files via upload

---
+++ Computer Science/Projects/Ch4_Program2_Solution/Ch4_Program2/Ch4_Program2.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Threading;
+
+namespace Ch4_Program2.ChapterFour
+{
+    public class Ch4_Program2 : Colors
+    {
+        static void Main(string[] args)
+        {
+            Responses.Header();
+            MagentaFont();                                           //Magenta coloring
+            Console.WriteLine("Would you like to take this survey? (yes/no)\n");
+            YellowFont();                                            //Yellow coloring
+            string yesOrNo = Convert.ToString(Console.ReadLine());
+            string yesOrNoFormatting = yesOrNo.ToLower();
+
+            #region No
+            if (yesOrNoFormatting == "no")  //Answer is no
+            {
+                Responses.AnswerIsNo();
+            }
+            #endregion
+
+            #region No Shortened
+            else if (yesOrNoFormatting == "n")  //Answer is no
+            {
+                Responses.AnswerIsNo();
+            }
+            #endregion
+
+            #region Yes
+            else if (yesOrNoFormatting == "yes") //Answer is yes
+            {
+                Responses.QuestionTwo();                                      //Second Question of the survey
+                string favoriteSaying = Convert.ToString(Console.ReadLine());
+                Loading();                                                     //Loading Process
+                Responses.EndSurveyGivenAnswer();                              //Header and responses
+                Console.Write("{0}\n\n", favoriteSaying);
+                Responses.EndSurvey();                                         //Ending survey message
+            }
+            #endregion
+
+            #region Yes Shortened
+            else if (yesOrNoFormatting == "y") //Answer is yes
+            {
+                Responses.QuestionTwo();                                       //Second Question of the survey
+                string favoriteSaying = Convert.ToString(Console.ReadLine());
+                Loading();                                                     //Loading Process
+                Responses.EndSurveyGivenAnswer();                              //Header and responses
+                Console.Write("{0}\n\n", favoriteSaying);
+                Responses.EndSurvey();                                         //Ending survey message
+            }
+            #endregion
+
+            #region Invalid Answer
+            else
+            {
+                Responses.AnswerIsInvalid();                                  //Invalid Answer Sequence
+            }
+            #endregion
+
+        }
+
+        public static void Loading()
+        {
+            CyanFont(); //Cyan Coloring
+            Console.Write("\n\nLoading Results");
+            Thread.Sleep(500);
+            Console.Write(".");
+            Thread.Sleep(500);
+            Console.Write(".");
+            Thread.Sleep(500);
+            Console.Write(".");
+            Thread.Sleep(500);
+            Console.Clear();
+        }
+    }
+}
---
+++ Computer Science/Projects/Ch4_Program2_Solution/Ch4_Program2/Colors.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Ch4_Program2.ChapterFour
+{
+    public class Colors
+    {
+        public static void CyanFont()
+        {
+            Console.ResetColor();
+            Console.ForegroundColor = ConsoleColor.Cyan;
+        }
+        public static void MagentaFont()
+        {
+            Console.ResetColor();
+            Console.ForegroundColor = ConsoleColor.Magenta;
+        }
+        public static void YellowFont()
+        {
+            Console.ResetColor();
+            Console.ForegroundColor = ConsoleColor.Yellow;
+        }
+    }
+}
