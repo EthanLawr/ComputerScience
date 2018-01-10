f6599d9b5dd41d30f910af75a3660077a3b28f2f
Ethan <elawrence152@gmail.com>
2018-01-10 10:49:59 -0500

    Add files via upload

---
+++ Administrator Comp Sci 1/2017-2018/Day2_Per78_MindGrenade_Solution/Day2_Per78_MindGrenade/CommonTasks.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day2_Per78_MindGrenade
+{
+    class CommonTasks
+    {
+        public static void HeaderB(string x, string y)//string x is a placeholder
+        {
+            Console.WriteLine("Author: Me");
+            Console.WriteLine("Date: {0}",x);
+            Console.WriteLine("Purpose . . .{0} ",y);
+
+        }
+    }
+}
