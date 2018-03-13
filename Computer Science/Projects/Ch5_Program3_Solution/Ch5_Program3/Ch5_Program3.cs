f3729dc11f318f17ccdd4392054f2d7493739328
Ethan <elawrence152@gmail.com>
2018-03-13 11:43:38 -0400

    Add files via upload

---
+++ Computer Science/Projects/Ch5_Program3_Solution/Ch5_Program3/Ch5_Program3.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+
+namespace Ch5_Program3
+{
+    class Ch5_Program3
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 0; i < 1; i++)                                  //Error looping
+            {
+                try
+                {
+                    TaskShortener.Header();
+                    Thread tid1 = new Thread(new ThreadStart(Thread1));
+                    Thread tid2 = new Thread(new ThreadStart(Thread2));
+
+                    tid1.Start();
+                    tid2.Start();
+                    TaskShortener.TetrisSong();
+                    TaskShortener.Footer();
+                    break;
+                }
+                catch (FormatException) //Trying to error it eh?
+                {
+                    TaskShortener.ErrorCatch();
+                }
+
+            }
+        }
+        public static void Thread1()
+        {
+            for (int i = 1; i <= 10; i++)
+            {
+                Console.Write(string.Format("Thread1 {0}", i));
+            }
+        }
+
+        public static void Thread2()
+        {
+            for (int i = 1; i <= 10; i++)
+            {
+                Console.Write(string.Format("Thread2 {0}", i));
+            }
+        }
+    }
+}
---
+++ Computer Science/Projects/Ch5_Program3_Solution/Ch5_Program3/Colors.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Colorful;
+using Console = System.Console;
+using CConsole = Colorful.Console;
+using System.Drawing;
+
+namespace Ch5_Program3
+{
+    class Colors
+    {
+        internal static void CyanFont()
+        {
+            Console.ResetColor(); CConsole.ForegroundColor = Color.LightSkyBlue;
+        }
+
+        internal static void RedFont()
+        {
+            Console.ResetColor(); CConsole.ForegroundColor = Color.Tomato;
+        }
+
+        internal static void PinkFont()
+        {
+            Console.ResetColor(); CConsole.ForegroundColor = Color.HotPink;
+        }
+
+        internal static void YellowFont()
+        {
+            Console.ResetColor(); CConsole.ForegroundColor = Color.Wheat;
+        }
+
+        internal static void GreenFont()
+        {
+            Console.ResetColor(); CConsole.ForegroundColor = Color.SpringGreen;
+        }
+    }
+}
