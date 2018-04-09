8c86bcc062b3f8281059b646c06731dd1bf815a8
Ethan <elawrence152@gmail.com>
2018-04-09 11:37:13 -0400

    Add files via upload

---
+++ Computer Science/Projects/Ch5_Program5_Solution/Ch5_Program5/Program5.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+
+namespace Ch5_Program5
+{
+    class Program5 : TaskShortener
+    {
+        static void Main(string[] args)
+        {
+            #region Threads
+            Thread tid1 = new Thread(new ThreadStart(TetrisSong));
+            tid1.Start(); //Runs a song while you run the program.
+            #endregion
+            for (int i = 0; i < 1; i++)                                  //Error looping
+            {
+                try
+                {
+
+                    ConsoleChangeSize();
+                    Header();
+
+                    #region Important Code
+                    double thing = AskUserForDouble("the cost");
+                    #endregion
+
+                    Footer();
+                    break;
+                }
+                #region Catch
+                catch (FormatException) //Trying to error it eh?
+                {
+                    Thread.Sleep(300);
+                    Console.Clear();
+                    ErrorFont(); //Red!
+                    TypeLine("\n\n\n\n\n\n\n\n\n\n\t\t\t\tTry again Knucklehead.");
+                    //Try again buddy. Do not be a knucklehead.
+                    Thread.Sleep(500); //Oh yes. You must wait to return you knucklehead.
+                    Console.Clear();
+                }
+                #endregion
+            }
+        }
+    }
+}
