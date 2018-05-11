7ab9fa0949aba9f0caf10e12bc44bb3f035a3799
Ethan <28376972+Evildeathpro@users.noreply.github.com>
2018-05-11 11:18:07 -0400

    Add files via upload

---
+++ Computer Science/Projects/Ch6_Program2_Solution/Ch6_Program2/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+
+namespace Ch6_Program2
+{
+    class Program : TaskShortener
+    {
+        static void Main(string[] args)
+        {
+            ConsoleChangeSize();
+            #region Threads
+            Thread tid1 = new Thread(new ThreadStart(MarioSong));
+            tid1.Start(); //Runs a song while you run the program.
+            #endregion
+            for (int i = 0; i < 1; i++)                                  //Error looping
+            {
+                try
+                {
+                    Header();
+
+                    #region Important Code
+                    int sideOne = AskUserForInt("one side of a triangle"),
+                        sideTwo = AskUserForInt("a second side of a triangle"),
+                    sideThree = AskUserForInt("a third side of a triangle");
+                    PythagoreanTest(sideOne, sideTwo, sideThree);
+                    #endregion
+
+                    Footer();
+                }
+                #region Catch
+                catch (FormatException) //Trying to error it eh?
+                {
+                    Thread.Sleep(300);
+                    Console.Clear();
+                    ErrorFont(); //Red!
+                    TypeLine("\n\n\n\n\n\n\n\n\n\n\t\t\t\tTry again Knucklehead.");
+                    //Try again buddy. Do not be a knucklehead.
+                    Thread.Sleep(500); //Oh yes. You must wait to return you knucklehead.
+                    Console.Clear();
+                }
+                #endregion
+            }
+        }
+        internal static void PythagoreanTest(int x, int y, int z)
+        {
+            int
+        }
+    }
+}
