ae7f072837ecac1715eccaf8a17e1b00f84f7f04
Ethan <28376972+Evildeathpro@users.noreply.github.com>
2018-05-18 11:22:03 -0400

    Delete s.js

--- SideProjectsJavaScript/Chips-Test1/commands/s.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  name: "s",
-  async func(msg, { send, author, content }) {
-    msg.delete();
-    if (author.id != Constants.users.WILLYZ) return;
-    content = content.substr(content.indexOf(" ") + 1);
-    send(content);
-  }
-};
