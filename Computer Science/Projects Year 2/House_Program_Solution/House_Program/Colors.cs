35878b0b85a8295c3a524c1d906f549af8679b78
Ethan <28376972+Evildeathpro@users.noreply.github.com>
2018-09-18 11:41:19 -0400

    Add files via upload

---
+++ Computer Science/Projects Year 2/House_Program_Solution/House_Program/Colors.cs
@@ -0,0 +1,56 @@
+﻿using Console = System.Console;
+using CConsole = Colorful.Console;
+using System.Drawing;
+
+// Install-Package Colorful.Console  -ProjectName House_Program
+namespace House_Program
+{
+    class Colors
+    {
+        internal static void CyanFont()
+        {
+            Console.ResetColor(); CConsole.ForegroundColor = Color.LightSkyBlue;
+        }
+
+        internal static void RedFont()
+        {
+            Console.ResetColor(); CConsole.ForegroundColor = Color.Tomato;
+        }
+
+        internal static void PinkFont()
+        {
+            Console.ResetColor(); CConsole.ForegroundColor = Color.HotPink;
+        }
+
+        internal static void YellowFont()
+        {
+            Console.ResetColor(); CConsole.ForegroundColor = Color.Wheat;
+        }
+
+        internal static void GreenFont()
+        {
+            Console.ResetColor(); CConsole.BackgroundColor = Color.CadetBlue; CConsole.ForegroundColor = Color.SpringGreen;
+        }
+        internal static void WhiteFont()
+        {
+            Console.ResetColor(); CConsole.BackgroundColor = Color.White; CConsole.ForegroundColor = Color.SandyBrown;
+        }
+        internal static void ErrorFont()
+        {
+            Console.ResetColor(); CConsole.ForegroundColor = Color.Crimson;
+        }
+        internal static void BrownFont()
+        {
+            Console.ResetColor(); CConsole.ForegroundColor = Color.SandyBrown;
+        }
+        internal static void skyFont()
+        {
+            Console.ResetColor(); CConsole.ForegroundColor = Color.WhiteSmoke; CConsole.BackgroundColor = Color.CadetBlue;
+        }
+        internal static void skyHouseFont()
+        {
+            Console.ResetColor(); CConsole.ForegroundColor = Color.SandyBrown; CConsole.BackgroundColor = Color.CadetBlue;
+        }
+
+    }
+}
---
+++ Computer Science/Projects Year 2/House_Program_Solution/House_Program/Housing.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+
+namespace House_Program
+{
+    class Housing : TaskShortener
+    {
+        static void Main(string[] args)
+        {
+            #region Threads
+            Thread tid1 = new Thread(new ThreadStart(MarioSong));
+            tid1.Start(); //Runs a song while you run the program.
+            #endregion
+            for (int i = 0; i < 1; i++)                                  //Error looping
+            {
+                try
+                {
+                    Header();
+                    #region Important Code
+                    skyFont();
+                    Console.WriteLine("######                                      ");
+                    Console.WriteLine("####           # # #                        ");
+                    Console.WriteLine("###             # #                         ");
+                    Console.WriteLine("#                #                          ");
+                    Console.WriteLine("                 #                          ");
+                    skyHouseFont();
+                    Console.WriteLine("                 H                          ");
+                    Console.WriteLine("               _H________________________   ");
+                    GreenFont();
+                    Console.Write("     @@@      ");
+                    WhiteFont();
+                    Console.WriteLine("/                         /1  ");
+                    GreenFont();
+                    Console.Write("    @@@@@    ");
+                    WhiteFont();
+                    Console.WriteLine("/                         / l  ");
+                    GreenFont();
+                    Console.Write("    @@@@@   ");
+                    WhiteFont();
+                    Console.WriteLine("l~~~~~~~~~~~~~~~~~~~~~~~~~l~~l  ");
+                    GreenFont();
+                    Console.Write("     @@@    ");
+                    WhiteFont();
+                    Console.WriteLine("l                         l  l  ");
+                    skyHouseFont();
+                    Console.Write("      l     ");
+                    WhiteFont();
+                    Console.WriteLine("l                         l  l  ");
+                    skyHouseFont();
+                    Console.Write("      l     ");
+                    WhiteFont();
+                    Console.WriteLine("l              __         l  l  ");
+                    skyHouseFont();
+                    Console.Write("      l     ");
+                    WhiteFont();
+                    Console.WriteLine("l             l  l        l  l  ");
+                    skyHouseFont();
+                    Console.Write("      l     ");
+                    WhiteFont();
+                    Console.WriteLine("l             l .l        l _l  ");
+                    skyHouseFont();
+                    Console.Write("      l     ");
+                    WhiteFont();
+                    Console.Write("l_____________L__L________l/    ");
+
+                    #endregion
+                    Footer();
+                }
+                #region Catch
+                catch (FormatException) //Trying to error it eh?
+                {
+                    Thread.Sleep(300);
+                    Console.Clear();
+                    ErrorFont(); //Red!
+                    TypeLine("\n\n\n\n\n\n\n\n\n\n\t\t\t\tTry again Knucklehead.");
+                    //Try again buddy. Do not be a knucklehead.
+                    Thread.Sleep(500); //Oh yes. You must wait to return you knucklehead.
+                    Console.Clear();
+                }
+                #endregion
+            }
+        }
+
+    }
+}
