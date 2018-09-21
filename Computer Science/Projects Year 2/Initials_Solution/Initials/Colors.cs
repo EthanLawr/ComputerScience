abc923bea9dfaaa2aca73eb14414b5728a397822
Ethan <28376972+Evildeathpro@users.noreply.github.com>
2018-09-21 11:31:10 -0400

    Add files via upload

---
+++ Computer Science/Projects Year 2/Initials_Solution/Initials/Colors.cs
@@ -0,0 +1,38 @@
+﻿using Console = System.Console;
+using CConsole = Colorful.Console;
+using System.Drawing;
+//Install-Package Colorful.Console  -ProjectName Initials
+namespace Initials
+{
+    class Colors
+    {
+        internal static void CyanFont()
+        {
+            Console.ResetColor(); CConsole.ForegroundColor = Color.LightSkyBlue;
+        }
+
+        internal static void RedFont()
+        {
+            Console.ResetColor(); CConsole.ForegroundColor = Color.Tomato;
+        }
+
+        internal static void PinkFont()
+        {
+            Console.ResetColor(); CConsole.ForegroundColor = Color.HotPink;
+        }
+
+        internal static void YellowFont()
+        {
+            Console.ResetColor(); CConsole.ForegroundColor = Color.Wheat;
+        }
+
+        internal static void GreenFont()
+        {
+            Console.ResetColor(); CConsole.ForegroundColor = Color.SpringGreen;
+        }
+        internal static void ErrorFont()
+        {
+            Console.ResetColor(); CConsole.ForegroundColor = Color.Crimson;
+        }
+    }
+}
---
+++ Computer Science/Projects Year 2/Initials_Solution/Initials/Ehl.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+
+namespace Initials
+{
+    class Ehl : TaskShortener
+    {
+        static void Main(string[] args)
+        {
+            #region Threads
+            Thread tid1 = new Thread(new ThreadStart(MarioSong));
+            tid1.Start(); //Runs a song while you run the program.
+            #endregion
+            for (int i = 0; i < 1; i++)                                  //Error looping
+            {
+                try
+                {
+                    Header();
+                    #region Important Code
+                    Console.WriteLine("EEEEEEE HH    HH LL\nEE\tHH    HH LL\nEEEEE\tHHHHHHHH LL\nEE\tHH    HH LL\nEEEEEEE HH    HH LLLLLLL\n");
+                    #endregion
+                    Footer();
+                }
+                #region Catch
+                catch (FormatException) //Trying to error it eh?
+                {
+                    Thread.Sleep(300);
+                    Console.Clear();
+                    ErrorFont(); //Red!
+                    TypeLine("\n\n\n\n\n\n\n\n\n\n\t\t\t\tTry again Knucklehead.");
+                    //Try again buddy. Do not be a knucklehead.
+                    Thread.Sleep(500); //Oh yes. You must wait to return you knucklehead.
+                    Console.Clear();
+                }
+                #endregion
+            }
+        }
+    }
+}
