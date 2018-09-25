25c2ea5a006908f530015f1b6c3e55841a136e89
Ethan <28376972+Evildeathpro@users.noreply.github.com>
2018-09-25 11:33:09 -0400

    Add files via upload

---
+++ Computer Science/Projects Year 2/Lyrics_Solution/Lyrics/LyricWriting.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+
+namespace Lyrics
+{
+    class LyricWriting : TaskShortener
+    {
+        static void Main(string[] args)
+        {
+            ConsoleChangeSize();
+            #region Threads
+            Thread tid1 = new Thread(new ThreadStart(MarioSong));
+            tid1.Start(); //Runs a song while you run the program.
+            #endregion
+            for (int i = 0; i < 1; i++)                                  //Error looping
+            {
+                try
+                {
+                    Header();
+                    #region Important Code
+                    Console.WriteLine("You\n");
+                    Console.WriteLine("gotta\n");
+                    Console.WriteLine("start\n");
+                    Console.WriteLine("to\n");
+                    Console.WriteLine("run\n");
+                    Console.WriteLine("before\n");
+                    Console.WriteLine("you\n");
+                    Console.WriteLine("learn\n");
+                    Console.WriteLine("to\n");
+                    Console.WriteLine("crawl\n");
+                    #endregion
+                    Footer();
+                }
+                #region Catch
+                catch (FormatException) //Trying to error it eh?
+                {
+                    Thread.Sleep(300);
+                    Console.Clear();
+                    ErrorFont(); //Red!
+                    TypeLine("\n\n\n\n\n\n\n\n\n\n\t\t\t\tTry again Knucklehead.");
+                    //Try again buddy. Do not be a knucklehead.
+                    Thread.Sleep(500); //Oh yes. You must wait to return you knucklehead.
+                    Console.Clear();
+                }
+                #endregion
+            }
+        }
+    }
+}
