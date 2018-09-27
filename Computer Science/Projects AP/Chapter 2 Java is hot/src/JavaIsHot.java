659bf395adbc0986e2a0268909e623c3fb7556ec
Ethan <28376972+Evildeathpro@users.noreply.github.com>
2018-09-27 08:17:42 -0400

    Update JavaIsHot.java

--- Computer Science/Projects AP/Chapter 2 Java is hot/src/JavaIsHot.java
+++ Computer Science/Projects AP/Chapter 2 Java is hot/src/JavaIsHot.java
@@ -4,19 +4,19 @@ public class JavaIsHot {
         // By Ethan Lawrence 9/27/2018
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("       d ");
-		System.out.println(" o      l ");
-		System.out.println("  l      r ");
-		System.out.println("   l    o ");
-		System.out.println("  e      W ");
-		System.out.println(" H ");
-		System.out.println(" xxxxxxxxxxxxxxxxxx ");
-		System.out.println(" x              x x ");
-		System.out.println(" x     Java     x x ");
-		System.out.println(" x              xxx ");
-		System.out.println(" x    is hot!   x ");
-		System.out.println(" x             x ");
-		System.out.println(" xxxxxxxxxxxxxx ");
+		System.out.println("         d");
+		System.out.println("   o      l");
+		System.out.println("    l      r");
+		System.out.println("     l    o");
+		System.out.println("    e      W");
+		System.out.println("   H");
+		System.out.println(" xxxxxxxxxxxxxxxxxx");
+		System.out.println(" x              x x");
+		System.out.println(" x     Java     x x");
+		System.out.println(" x              xxx");
+		System.out.println(" x    is hot!   x");
+		System.out.println(" x             x");
+		System.out.println(" xxxxxxxxxxxxxx");
 	}
 
 }
