c7c9dabc8049259883f8acf736c4d2c5fccffb73
Ethan <28376972+Evildeathpro@users.noreply.github.com>
2018-09-27 08:09:51 -0400

    Add files via upload

---
+++ Computer Science/Projects AP/Chapter 2 Hello World/src/HelloWorld.java
@@ -0,0 +1,13 @@
+
+public class HelloWorld {
+    // This program will say Hello World!!!!!!
+	// By Ethan Lawrence 9/27/2018
+	// 
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+            System.out.println("Hello World!!!!!!"); 
+            // Printing to the console for a basic Hello World program
+            
+	}
+
+}
