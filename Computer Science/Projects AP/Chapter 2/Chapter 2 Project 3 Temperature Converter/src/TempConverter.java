01b06e06d35a58fb830f802a037ed1c0563bc820
Ethan <28376972+Evildeathpro@users.noreply.github.com>
2018-10-01 07:53:27 -0400

    Add files via upload

--- Computer Science/Projects AP/Chapter 2/Chapter 2 Project 3 Temperature Converter/src/TempConverter.java
+++ Computer Science/Projects AP/Chapter 2/Chapter 2 Project 3 Temperature Converter/src/TempConverter.java
@@ -1,18 +1,19 @@
 import java.util.Scanner;
 public class TempConverter {
-	// This program will say Hello World with a mug!
-    // By Ethan Lawrence 9/27/2018
+	// This program will convert Fahrenheit to Celsius with User Input
+    // By Ethan Lawrence 10/1/2018
 	// elawrence152@gmail.com
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-        Scanner reader = new Scanner(System.in);
+		Scanner reader = new Scanner(System.in);
         double fahrenheit;
         double celsius;
         
-        System.out.print("Enter degrees Fahrenheit:");
+        System.out.println("This program will convert degrees Fahrenheit to degrees Celsius");
+        System.out.print("Enter degrees Fahrenheit: ");
         fahrenheit = reader.nextDouble();
-        celsius = (fahrenheit - 32.0)*5.0/9.0;
-        System.out.print("The equivalent in Celsious is ");
+        celsius = (fahrenheit - 32.0) * 5.0 / 9.0;
+        System.out.print("The equivalent in Celsius is ");
         System.out.println(celsius);
 	}
 
