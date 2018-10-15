f7b5ce33f5fca472c2957b19bc75f3de26bc58bb
Ethan <28376972+Evildeathpro@users.noreply.github.com>
2018-10-15 11:01:49 -0400

    Add files via upload

---
+++ Computer Science/Projects Year 2/Chapter2_GuessingGame_Solution/Chapter2_GuessingGame/RandomGuess.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+
+namespace Chapter2_GuessingGame
+{
+    class RandomGuess : TaskShortener
+    {
+        static void Main(string[] args)
+        {
+            ConsoleChangeSize();
+            #region Threads
+            Thread tid1 = new Thread(new ThreadStart(MarioSong));
+            tid1.Start(); //Runs a song while you run the program.
+            #endregion
+            for (int i = 0; i < 1; i++)                                  //Error looping
+            {
+                try
+                {
+                    Header();
+                    #region Important Code
+                    Random random = new Random();
+
+                    int someNumber = random.Next(1, 100);
+
+                    Console.WriteLine("There is a random integer between 0 and 100. Please try to guess what it is!");
+                    int userGuess = AskUserForInt("a number.");
+                    while (userGuess != someNumber)
+                    {
+                        while (userGuess < someNumber)
+                        {
+                            Console.WriteLine($"Sorry! That's incorrect! The answer is higher than {userGuess}");
+                            userGuess = AskUserForInt("another number.");
+                        }
+                        while (userGuess > someNumber)
+                        {
+                            Console.WriteLine($"Sorry! That's incorrect! The answer is lower than {userGuess}.  Please try again!");
+                            userGuess = AskUserForInt("another number.");
+                        }
+                    }
+                    if (userGuess == someNumber) Console.WriteLine($"Great job! The answer was {someNumber}.");
+                    #endregion
+                    Footer();
+                }
+                #region Catch
+                catch (FormatException) //Trying to error it eh?
+                {
+                    Thread.Sleep(300);
+                    Console.Clear();
+                    ErrorFont(); //Red!
+                    TypeLine("\n\n\n\n\n\n\n\n\n\n\t\t\t\tTry again Knucklehead.");
+                    //Try again buddy. Do not be a knucklehead.
+                    Thread.Sleep(500); //Oh yes. You must wait to return you knucklehead.
+                    Console.Clear();
+                }
+                #endregion
+            }
+        }
+    }
+}
---
+++ Computer Science/Projects Year 2/Chapter2_UlamHypothesis_Solution/Chapter2_UlamHypothesis/UlamHypothesisPrgm.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+
+namespace Chapter2_UlamHypothesis
+{
+    class UlamHypothesisPrgm : TaskShortener
+    {
+        static void Main(string[] args)
+        {
+            ConsoleChangeSize();
+            #region Threads
+            Thread tid1 = new Thread(new ThreadStart(MarioSong));
+            tid1.Start(); //Runs a song while you run the program.
+            #endregion
+            for (int i = 0; i < 1; i++)                                  //Error looping
+            {
+                try
+                {
+                    Header();
+                    #region Important Code
+                    int j, k;
+                    for (j = 1; j <= 25; j++)
+                    {
+                        if (j > 9) Console.Write("{0} |", j); // Lists numbers for organization
+                        else Console.Write("{0}  |", j);
+                        k = j;
+                        if (k % 2 == 1) // Even
+                        {
+                            k = k * 3 + 1;
+                            Console.Write(" {0}", k); 
+                        }
+                        while (k % 2 == 0 && k != 1) // Odd
+                        {
+                            k = k / 2;
+                            Console.Write(" {0}", k);
+                        }
+                        Console.WriteLine();
+                    }
+                        #endregion
+                        Footer();
+                }
+                #region Catch
+                catch (FormatException) //Trying to error it eh?
+                {
+                    Thread.Sleep(300);
+                    Console.Clear();
+                    ErrorFont(); //Red!
+                    TypeLine("\n\n\n\n\n\n\n\n\n\n\t\t\t\tTry again Knucklehead.");
+                    //Try again buddy. Do not be a knucklehead.
+                    Thread.Sleep(500); //Oh yes. You must wait to return you knucklehead.
+                    Console.Clear();
+                }
+                #endregion
+            }
+        }
+    }
+}
