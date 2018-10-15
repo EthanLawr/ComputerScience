ed1bc4cd6be5b3ffcbdc276dfb5a75821ebfaf1c
Ethan <28376972+Evildeathpro@users.noreply.github.com>
2018-10-15 08:11:35 -0400

    Add files via upload

--- Computer Science/Projects AP/Chapter 3/Chapter 3 Lab#3 BatAvg/src/BatAvg.java
+++ Computer Science/Projects AP/Chapter 3/Chapter 3 Lab#3 BatAvg/src/BatAvg.java
@@ -7,13 +7,13 @@ public class BatAvg {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Scanner userInput = new Scanner(System.in);
-		System.out.println("This program will find the batting average of someone.");
+		System.out.println("This program will find the batting average of a baseball player.");
 		System.out.print("Please enter the number of bats (Integer): ");
 		int bats = userInput.nextInt();
         System.out.print("Please enter the number of hits (Integer): ");
         int hits = userInput.nextInt();
         double battingAverage = (double)((int)(((double)bats)/((double)hits)*1000))/1000;
-        System.out.println("The batting average for someone with " + bats + " bats and " +
+        System.out.println("The batting average for a baseball player with " + bats + " bats and " +
         hits + " hits is: " + battingAverage);
 	}
 
