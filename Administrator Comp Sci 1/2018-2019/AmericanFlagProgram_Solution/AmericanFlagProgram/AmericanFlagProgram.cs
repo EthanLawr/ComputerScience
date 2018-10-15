9b94aac9d5a1ec2ac3d95ce3c6859161df607acf
Ethan <28376972+Evildeathpro@users.noreply.github.com>
2018-10-15 07:59:13 -0400

    Add files via upload

---
+++ Administrator Comp Sci 1/2018-2019/AmericanFlagProgram_Solution/AmericanFlagProgram/AmericanFlagProgram.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AmericanFlagProgram
+{
+    class AmericanFlagProgram
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("American Flag Program");
+            Console.WriteLine("Mr. Lynch\t\tDate: 9/18/18");
+            Console.WriteLine("**********************************"
+                + "**********\n\n\n");
+
+            //Blue background and White stars with Red stripe
+            Console.BackgroundColor = ConsoleColor.Blue;
+            Console.Write("*\0*\0*\0*\0*\0*\0*\0*");
+            Console.BackgroundColor = ConsoleColor.Red;
+            Console.WriteLine("\t\t\t\t\t");
+            //Blue background and white stars with White stripe
+            Console.BackgroundColor = ConsoleColor.Blue;
+            Console.Write("\0*\0*\0*\0*\0*\0*\0*\0");
+            Console.BackgroundColor = ConsoleColor.White;
+            Console.WriteLine("\t\t\t\t\t");
+
+            //Blue background and White stars with Red stripe
+            Console.BackgroundColor = ConsoleColor.Blue;
+            Console.Write("*\0*\0*\0*\0*\0*\0*\0*");
+            Console.BackgroundColor = ConsoleColor.Red;
+            Console.WriteLine("\t\t\t\t\t");
+            //Blue background and white stars with White stripe
+            Console.BackgroundColor = ConsoleColor.Blue;
+            Console.Write("\0*\0*\0*\0*\0*\0*\0*\0");
+            Console.BackgroundColor = ConsoleColor.White;
+            Console.WriteLine("\t\t\t\t\t");
+
+            //Blue background and White stars with Red stripe
+            Console.BackgroundColor = ConsoleColor.Blue;
+            Console.Write("*\0*\0*\0*\0*\0*\0*\0*");
+            Console.BackgroundColor = ConsoleColor.Red;
+            Console.WriteLine("\t\t\t\t\t");
+            //Blue background and white stars with White stripe
+            Console.BackgroundColor = ConsoleColor.Blue;
+            Console.Write("\0*\0*\0*\0*\0*\0*\0*\0");
+            Console.BackgroundColor = ConsoleColor.White;
+            Console.WriteLine("\t\t\t\t\t");
+
+            //Blue background and White stars with Red stripe
+            Console.BackgroundColor = ConsoleColor.Blue;
+            Console.Write("*\0*\0*\0*\0*\0*\0*\0*");
+            Console.BackgroundColor = ConsoleColor.Red;
+            Console.WriteLine("\t\t\t\t\t");
+
+            //Long White stripe
+            Console.BackgroundColor = ConsoleColor.White;
+            Console.WriteLine("\t\t\t\t\t\t");
+            //Long Red stripe
+            Console.BackgroundColor = ConsoleColor.Red;
+            Console.WriteLine("\t\t\t\t\t\t");
+
+            //Long White stripe
+            Console.BackgroundColor = ConsoleColor.White;
+            Console.WriteLine("\t\t\t\t\t\t");
+            //Long Red stripe
+            Console.BackgroundColor = ConsoleColor.Red;
+            Console.WriteLine("\t\t\t\t\t\t");
+
+            //Long White stripe
+            Console.BackgroundColor = ConsoleColor.White;
+            Console.WriteLine("\t\t\t\t\t\t");
+            //Long Red stripe
+            Console.BackgroundColor = ConsoleColor.Red;
+            Console.WriteLine("\t\t\t\t\t\t");
+
+           
+            
+
+            Console.Read();
+        }
+    }
+}
