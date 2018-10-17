ae2b3c7a14a4ba0bf4ca29b9307199b6224ffab5
Ethan <28376972+Evildeathpro@users.noreply.github.com>
2018-10-17 08:30:15 -0400

    Add files via upload

--- Computer Science/Projects AP/Chapter 3/Chapter 3 Lab#6 GUIMoon/src/GUIMoon.java
+++ Computer Science/Projects AP/Chapter 3/Chapter 3 Lab#6 GUIMoon/src/GUIMoon.java
@@ -5,14 +5,14 @@ import java.awt.Graphics;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 
-public class HelloGUIWorld {
-	// This program will make text in a GUI window
+public class GUIMoon {
+	// This program will make a moon in a GUI Window
     // By Ethan Lawrence 10/17/2018
 	// elawrence152@gmail.com
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
         JFrame theGUI = new JFrame();
-        theGUI.setTitle("Hello GUI World Program");
+        theGUI.setTitle("GUI Moon Program");
         theGUI.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         theGUI.setSize(300, 300);
         theGUI.add(new ColorPanel());
@@ -26,9 +26,9 @@ class ColorPanel extends JPanel {
 	}
     public void paintComponent(Graphics g) {
     	super.paintComponent(g);
-    	g.drawRoundRect(10, 10, 200, 200, 30, 30);
-    	Font font = new Font("Arial", Font.PLAIN, 12);
-    	g.setFont(font);
-    	g.drawString("Hello GUI World!", 30, 30);
+    	g.setColor(Color.lightGray);
+    	g.fillOval(10, 10, 150, 150);
+    	g.setColor(Color.white);
+    	g.fillOval(50, 10, 150, 150);
     }
 }
