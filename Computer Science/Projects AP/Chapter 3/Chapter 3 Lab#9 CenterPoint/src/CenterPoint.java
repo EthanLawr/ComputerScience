2a4fbd673415d206c37f1308ab1824c89218c777
Ethan <28376972+Evildeathpro@users.noreply.github.com>
2018-10-23 12:04:15 -0400

    Update CenterPoint.java

--- Computer Science/Projects AP/Chapter 3/Chapter 3 Lab#9 CenterPoint/src/CenterPoint.java
+++ Computer Science/Projects AP/Chapter 3/Chapter 3 Lab#9 CenterPoint/src/CenterPoint.java
@@ -1,16 +1,11 @@
-import java.awt.Color;
-import java.awt.Font;
-import java.awt.Graphics;
-
-import javax.swing.JFrame;
-import javax.swing.JPanel;
+import java.awt.*
+import javax.swing.*
 
 public class CenterPoint {
 	// This program will make a center point on a GUI Window
-    // By Ethan Lawrence 10/18/2018
+        // By Ethan Lawrence 10/18/2018
 	// elawrence152@gmail.com
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
         JFrame theGUI = new JFrame();
         theGUI.setTitle("Center Point");
         theGUI.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -25,11 +20,11 @@ class ColorPanel extends JPanel {
 		setBackground(Color.DARK_GRAY);
 		setForeground(Color.cyan);
 	}
-    public void paintComponent(Graphics g) {
-    	super.paintComponent(g);
-    	int x = getWidth(), y = getHeight();
-    	Font font = new Font("Arial", Font.CENTER_BASELINE, 12);
-    	g.setFont(font);
-    	g.drawString("(" + x/2 + "," + y/2 + ")", x/2, y/2);
-    }
+	public void paintComponent(Graphics g) {
+		super.paintComponent(g);
+		int x = getWidth(), y = getHeight();
+		Font font = new Font("Arial", Font.CENTER_BASELINE, 12);
+		g.setFont(font);
+		g.drawString("(" + x/2 + "," + y/2 + ")", x/2, y/2);
+	}
 }
