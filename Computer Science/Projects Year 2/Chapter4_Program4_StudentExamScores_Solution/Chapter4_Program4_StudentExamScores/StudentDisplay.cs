52c12d70b892ba858dc34d5cab1eb1e95db5a96b
Ethan <28376972+Evildeathpro@users.noreply.github.com>
2018-11-02 11:16:19 -0400

    Add files via upload

---
+++ Computer Science/Projects Year 2/Chapter4_Program4_StudentExamScores_Solution/Chapter4_Program4_StudentExamScores/StudentDisplay.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chapter4_Program4_StudentExamScores
+{
+    class StudentDisplay
+    {
+        public StudentDisplay()
+        {
+            StudentInput student = new StudentInput();
+            Console.WriteLine($"Student ID: {student.StudentId}\nThe average score is {(student.TestOne+student.TestTwo+student.TestThree)/3.00}");
+
+        }
+    }
+}
---
+++ Computer Science/Projects Year 2/Chapter4_Program4_StudentExamScores_Solution/Chapter4_Program4_StudentExamScores/StudentInput.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chapter4_Program4_StudentExamScores
+{
+    class StudentInput : TaskShortener
+    {
+        private static string studentId;
+        private static double testScoreOne, testScoreTwo, testScoreThree;
+        public string StudentId
+        {
+            get
+            {
+                return studentId;
+            }
+            set
+            {
+                studentId = value;
+            }
+        }
+        public double TestOne
+        {
+            get
+            {
+                return testScoreOne;
+            }
+            set
+            {
+                testScoreOne =  value;
+            }
+        }
+        public double TestTwo
+        {
+            get
+            {
+                return testScoreTwo;
+            }
+            set
+            {
+                testScoreTwo = value;
+            }
+        }
+        public double TestThree
+        {
+            get
+            {
+                return testScoreThree;
+            }
+            set
+            {
+                testScoreThree = value;
+            }
+        }
+
+        public StudentInput()
+        {
+            studentId = AskUserForString("your student id.");
+            testScoreOne = AskUserForDouble("a test score");
+            testScoreTwo = AskUserForDouble("a second test score");
+            testScoreThree = AskUserForDouble("a third test score");
+        }
+    }
+}
