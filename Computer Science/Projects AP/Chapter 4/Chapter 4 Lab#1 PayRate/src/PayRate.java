cac8c5ea54610ce1920f768a7397c59337a0b779
Ethan <28376972+Evildeathpro@users.noreply.github.com>
2018-11-05 08:25:43 -0500

    Add files via upload

--- Computer Science/Projects AP/Chapter 4/Chapter 4 Lab#1 PayRate/src/PayRate.java
+++ Computer Science/Projects AP/Chapter 4/Chapter 4 Lab#1 PayRate/src/PayRate.java
@@ -4,7 +4,6 @@ public class PayRate {
     // By Ethan Lawrence 10/29/2018
 	// elawrence152@gmail.com
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
         System.out.println("This program will calculate the user's pay.");
         Scanner UserInput = new Scanner(System.in);
         System.out.print("Please enter how many hours you worked: ");
---
+++ Computer Science/Projects AP/Chapter 4/Chapter 4 Lab#2 RightTriangle/src/RightTriangle.java
@@ -0,0 +1,51 @@
+import java.util.Scanner;
+
+public class RightTriangle {
+	// This program will make a calculation for the information of a sphere
+    // By Ethan Lawrence 10/15/2018
+	// elawrence152@gmail.com
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Double hypotenuse, opposite, adjacent;
+		Scanner userInput = new Scanner(System.in);
+		String rightCheck = "not a right triangle.";
+		System.out.println("This program will find out what type of triangle 3 sides make.");
+		
+		// This block of code will collect a user input to get the side's lengths
+		System.out.print("Please enter the first side: ");
+		Double sideOne = userInput.nextDouble();
+		System.out.print("Please enter the second side: ");
+		Double sideTwo = userInput.nextDouble();
+		System.out.print("Please enter the third side: ");
+		Double sideThree = userInput.nextDouble();
+		
+		
+		// These if and else statements will find out the sides of a triangle and single out the hypotenuse.
+		if (sideOne > sideTwo && sideOne > sideThree) {
+			hypotenuse = sideOne;
+			opposite = sideTwo;
+			adjacent = sideThree;
+		} else if (sideTwo > sideOne && sideTwo > sideThree) {
+			hypotenuse = sideTwo;
+			opposite = sideOne;
+			adjacent = sideThree;
+		} else if (sideThree > sideOne && sideThree > sideTwo) {
+			hypotenuse = sideThree;
+			opposite = sideOne;
+			adjacent = sideTwo;
+		} else {
+			hypotenuse = sideOne;
+			opposite = sideTwo;
+			adjacent = sideThree;
+		}
+		
+		// These if statements will show if a triangle exists and what type of triangle it is if it does exist.
+		if (adjacent * adjacent + opposite * opposite == hypotenuse * hypotenuse) rightCheck = "a right triangle.";
+	    if (adjacent * adjacent + opposite * opposite > hypotenuse * hypotenuse) rightCheck = "an acute triangle.";
+	    if (adjacent * adjacent + opposite * opposite < hypotenuse * hypotenuse) rightCheck = "an obtuse triangle.";
+	    if (adjacent + opposite > hypotenuse) System.out.println("This triangle is " + rightCheck);
+	    else System.out.println("These sides do not make a triangle");
+        
+	}
+
+}
