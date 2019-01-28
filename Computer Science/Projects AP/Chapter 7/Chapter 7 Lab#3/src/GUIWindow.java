58fddc6378dd41acfa3459c1716f513fcd44f47a
Ethan <28376972+Evildeathpro@users.noreply.github.com>
2019-01-28 08:16:41 -0500

    Add files via upload

--- Computer Science/Projects AP/Chapter 7/Chapter 7 Lab#3/src/GUIWindow.java
+++ Computer Science/Projects AP/Chapter 7/Chapter 7 Lab#3/src/GUIWindow.java
@@ -3,6 +3,7 @@ import java.awt.Container;
 import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.Color;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
@@ -10,8 +11,17 @@ import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
 
+
 public class GUIWindow extends JFrame {
 
+	 private static Thermometer thermo = new Thermometer();
+	 private static JLabel fahrLabel = new JLabel("Degrees Fahrenheit");
+	 private static JLabel celsiusLabel = new JLabel("Degrees Celsius");
+	 private static JTextField fahrField = new JTextField("32.00");
+	 private static JTextField celsiusField = new JTextField("0.00");
+	 private static JButton fahrButton = new JButton("Convert >>>");
+	 private static JButton celsiusButton = new JButton("<<< Convert");
+	 
 	 public GUIWindow() {
 		 JPanel dataPanel = new JPanel(new GridLayout(3,3,12,6));
 		 dataPanel.add(fahrLabel);
@@ -28,33 +38,55 @@ public class GUIWindow extends JFrame {
 		 container.add(buttonPanel, BorderLayout.SOUTH);
 		 fahrButton.addActionListener(new FahrenheitListener());
 		 celsiusButton.addActionListener(new CelsiusListener());
-
 	 }
-	 private static Thermometer thermo = new Thermometer();
-	 private static JLabel fahrLabel = new JLabel("Degrees Fahrenheit");
-	 private static JLabel celsiusLabel = new JLabel("Degrees Celsius");
-	 private static JTextField fahrField = new JTextField("32.0");
-	 private static JTextField celsiusField = new JTextField("0.0");
-	 private static JButton fahrButton = new JButton("Convert >>>");
-	 private static JButton celsiusButton = new JButton("<<< Convert");
-
 	 
 	 private class FahrenheitListener implements ActionListener{
 			 public void actionPerformed(ActionEvent e) {
-				 String input = fahrField.getText();
-				 double fahr = Double.parseDouble(input);
-				 thermo.setFahrenheit(fahr);
-				 double celsius = thermo.getCelsius();
-				 celsiusField.setText(" " + celsius);
+				 try {
+					 celsiusField.setForeground(Color.BLACK);
+					 fahrField.setForeground(Color.BLACK);
+					 String input = fahrField.getText();
+					 double fahr = Double.parseDouble(input);
+					 thermo.setFahrenheit(fahr);
+					 double celsius = thermo.getCelsius();
+					 String formattedCelsius = String.format("%.2f", celsius); 
+					 celsiusField.setText(" " + formattedCelsius);
+					 String formattedFahr = String.format("%.2f", fahr); 
+					 celsiusField.setForeground(Color.BLUE);
+					 fahrField.setText(" " + formattedFahr);
+					 if (celsius < -273) {
+						 celsiusField.setText("Invalid Number!");
+						 celsiusField.setForeground(Color.RED);
+					 }
+				 } catch(Exception f) {
+					 celsiusField.setText("Invalid Number!");
+					 celsiusField.setForeground(Color.RED);
+				 }
+				 
 			 }
 	 }
 	 private class CelsiusListener implements ActionListener{
 		 public void actionPerformed(ActionEvent e) {
-			 String input = celsiusField.getText();
-			 double celsius = Double.parseDouble(input);
-			 thermo.setCelsius(celsius);
-			 double fahr = thermo.getFahrenheit();
-			 fahrField.setText(" " + fahr);
+			 try {
+				 celsiusField.setForeground(Color.BLACK);
+				 fahrField.setForeground(Color.BLACK);
+				 String input = celsiusField.getText();
+				 double celsius = Double.parseDouble(input);
+				 thermo.setCelsius(celsius);
+				 double fahr = thermo.getFahrenheit();
+				 String formattedCelsius = String.format("%.2f", celsius); 
+				 celsiusField.setText(" " + formattedCelsius);
+				 String formattedFahr = String.format("%.2f", fahr); 
+				 fahrField.setText(" " + formattedFahr);
+				 fahrField.setForeground(Color.BLUE);
+				 if (celsius < -273) {
+					 fahrField.setText("Invalid Number!");
+					 fahrField.setForeground(Color.RED);
+				 }
+			 } catch(Exception f) {
+				 fahrField.setText("Invalid Number!");
+				 fahrField.setForeground(Color.RED);
+			 }
 		 }
- }
+	 }
 }
