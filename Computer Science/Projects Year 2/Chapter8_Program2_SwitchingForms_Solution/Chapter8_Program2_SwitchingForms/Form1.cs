0a3dbadcaf83a8d5d0640ef14d5a65de00431dd4
Ethan <28376972+Evildeathpro@users.noreply.github.com>
2019-02-08 10:55:39 -0500

    Add files via upload

---
+++ Computer Science/Projects Year 2/Chapter8_Program2_SwitchingForms_Solution/Chapter8_Program2_SwitchingForms/Form1.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Chapter8_Program2_SwitchingForms
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            // New form class
+            Form2 f2 = new Form2();
+            f2.Show();
+            // Hides the old form
+            Hide();
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            // Stops the application
+            Close();
+            Application.Exit();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Form 1", "Form1", MessageBoxButtons.OK);
+            
+        }
+    }
+}
---
+++ Computer Science/Projects Year 2/Chapter8_Program2_SwitchingForms_Solution/Chapter8_Program2_SwitchingForms/Form2.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Chapter8_Program2_SwitchingForms
+{
+    public partial class Form2 : Form
+    {
+        public Form2()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Form 2", "Form2", MessageBoxButtons.OK);
+
+        }
+
+        internal void Form2_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            // New form class
+            Form1 f2 = new Form1();
+            f2.Show();
+            // Hides the old form
+            Hide();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            // Stops the applicaiton
+            Close();
+            Application.Exit();
+        }
+    }
+}
