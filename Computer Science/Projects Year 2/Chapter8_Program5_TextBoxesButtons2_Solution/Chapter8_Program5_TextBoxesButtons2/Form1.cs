3c612e88676b2d2508202b7787f2519925e9c981
Ethan <28376972+Evildeathpro@users.noreply.github.com>
2019-02-08 11:34:24 -0500

    Add files via upload

---
+++ Computer Science/Projects Year 2/Chapter8_Program5_TextBoxesButtons2_Solution/Chapter8_Program5_TextBoxesButtons2/Form1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Chapter8_Program5_TextBoxesButtons2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            // Keeps the box visible inside the monitor
+            if(int.Parse(textBox1.Text) > 1000 || int.Parse(textBox1.Text) < 0 || int.Parse(textBox2.Text) > 1000 || int.Parse(textBox2.Text) < 0)
+            {
+                // Do nothing
+            } else
+            // Changes location
+            Location = new Point(int.Parse(textBox1.Text), int.Parse(textBox2.Text));
+        }
+    }
+}
---
+++ Computer Science/Projects Year 2/Chapter8_Program6_GuessingGame_Solution/Chapter8_Program6_GuessingGame/Form1.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Chapter8_Program6_GuessingGame
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        public static Random r = new Random();
+        public static int target = r.Next(0, 101);
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            label1.Text = "Guess between 0 and 100!\nA blue button means you are too high!\nA green button means you are too low!";
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (int.Parse(textBox1.Text) > 100) MessageBox.Show("That is too high", "On no", MessageBoxButtons.OK);
+            else if (int.Parse(textBox1.Text) < 0) MessageBox.Show("That is too low", "On no", MessageBoxButtons.OK);
+            else if(int.Parse(textBox1.Text) == target)
+            {
+                // Win Condition
+                MessageBox.Show("You Won!", "Good Job", MessageBoxButtons.OK);
+                Close();
+            } else if (int.Parse(textBox1.Text) > target)
+            {
+                // Color change 1
+                button1.BackColor = Color.LightBlue;
+                
+            } else
+            {
+                // Color change 2
+                button1.BackColor = Color.LawnGreen;
+            }
+        }
+    }
+}
