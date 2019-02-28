9ddd9dbcb1cb67031eb224e4a6787e19f7812bd1
Ethan <28376972+Evildeathpro@users.noreply.github.com>
2019-02-28 11:34:28 -0500

    Add files via upload

---
+++ Computer Science/Projects Year 2/Chapter8_Program11_FlipBook_Solution/Chapter8_Program11_FlipBook/Form1.Designer.cs
@@ -0,0 +1,112 @@
+﻿namespace Chapter8_Program11_FlipBook
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 30F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(345, 95);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(196, 94);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Animate";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::Chapter8_Program11_FlipBook.Properties.Resources.pixil_frame_0;
+            this.pictureBox1.Location = new System.Drawing.Point(50, 95);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(271, 298);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 30F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.Location = new System.Drawing.Point(345, 207);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(196, 56);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Stop";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 30F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button3.Location = new System.Drawing.Point(345, 283);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(196, 56);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "Reset";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(566, 448);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+    }
+}
+
---
+++ Computer Science/Projects Year 2/Chapter8_Program11_FlipBook_Solution/Chapter8_Program11_FlipBook/Form1.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Chapter8_Program11_FlipBook
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            timer1.Tick += timer1_Tick;
+            timer1.Interval = 100;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            m += 30;
+            if (m > 2100)
+            {
+                // Resets the timer to loop the newly made "gif"
+                pictureBox1.Image = Properties.Resources.pixil_frame_0;
+                m = 0;
+            }
+            // Animates the pngs
+            else if (m > 2000) pictureBox1.Image = Properties.Resources.pixil_frame_20;
+            else if (m > 1900) pictureBox1.Image = Properties.Resources.pixil_frame_19;
+            else if (m > 1800) pictureBox1.Image = Properties.Resources.pixil_frame_18;
+            else if (m > 1700) pictureBox1.Image = Properties.Resources.pixil_frame_17;
+            else if (m > 1600) pictureBox1.Image = Properties.Resources.pixil_frame_16;
+            else if (m > 1500) pictureBox1.Image = Properties.Resources.pixil_frame_15;
+            else if (m > 1400) pictureBox1.Image = Properties.Resources.pixil_frame_14;
+            else if (m > 1300) pictureBox1.Image = Properties.Resources.pixil_frame_13;
+            else if (m > 1200) pictureBox1.Image = Properties.Resources.pixil_frame_12;
+            else if (m > 1100) pictureBox1.Image = Properties.Resources.pixil_frame_11;
+            else if (m > 1000) pictureBox1.Image = Properties.Resources.pixil_frame_10;
+            else if (m > 900) pictureBox1.Image = Properties.Resources.pixil_frame_9;
+            else if (m > 800) pictureBox1.Image = Properties.Resources.pixil_frame_8;
+            else if (m > 700) pictureBox1.Image = Properties.Resources.pixil_frame_7;
+            else if (m > 600) pictureBox1.Image = Properties.Resources.pixil_frame_6;
+            else if (m > 500) pictureBox1.Image = Properties.Resources.pixil_frame_5;
+            else if (m > 400) pictureBox1.Image = Properties.Resources.pixil_frame_4;
+            else if (m > 300) pictureBox1.Image = Properties.Resources.pixil_frame_3;
+            else if (m > 200) pictureBox1.Image = Properties.Resources.pixil_frame_2;
+            else if (m > 100) pictureBox1.Image = Properties.Resources.pixil_frame_1;
+
+
+        }
+        private static int m = 0;
+        private void button1_Click(object sender, EventArgs e)
+        {
+            // Check if the animation is going already
+            if (timer1.Enabled == true) MessageBox.Show("The animation has already started!");
+            else timer1.Start();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            // Check if the animation is already going
+            if (timer1.Enabled == false) MessageBox.Show("Please start the animation first!");
+            else timer1.Stop();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            // Checks if the program is reset
+            if (timer1.Enabled == false && pictureBox1.Image == Properties.Resources.pixil_frame_0) MessageBox.Show("This is already reset!");
+            else
+            {
+                // Resets the program
+                timer1.Stop();
+                pictureBox1.Image = Properties.Resources.pixil_frame_0;
+                m = 0;
+            }
+        }
+    }
+}
