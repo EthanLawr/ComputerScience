5108a1f467ebf5f37fef9d7e280d06c4344d5393
Ethan <28376972+Evildeathpro@users.noreply.github.com>
2019-03-05 11:31:07 -0500

    Add files via upload

---
+++ Computer Science/Projects Year 2/MultipleTopics_Program1_Uppercase_Solution/MultipleTopics_Program1_Uppercase/Form1.Designer.cs
@@ -0,0 +1,108 @@
+﻿namespace MultipleTopics_Program1_Uppercase
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 12);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(168, 20);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(20, 80);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(0, 13);
+            this.label1.TabIndex = 2;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F);
+            this.label2.Location = new System.Drawing.Point(12, 45);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(117, 26);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Uppercase";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F);
+            this.label3.Location = new System.Drawing.Point(248, 45);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(117, 26);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Lowercase";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(260, 80);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(0, 13);
+            this.label4.TabIndex = 5;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(613, 137);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ Computer Science/Projects Year 2/MultipleTopics_Program1_Uppercase_Solution/MultipleTopics_Program1_Uppercase/Form1.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MultipleTopics_Program1_Uppercase
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void contextMenuStrip1_Opening(object sender, CancelEventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            // Changes the actual text
+            label1.Text = Uppercase(textBox1.Text);
+            label4.Text = Lowercase(textBox1.Text);
+        }
+
+        public static string Uppercase(string x)
+        {
+            string y = "";
+            x.ToCharArray();
+            foreach(char s in x)
+            {
+                // Checks if already uppercase
+                if (Convert.ToInt32(s) - 32 >= 65 && Convert.ToInt32(s) - 32 <= 90)
+                    y += Convert.ToChar((Convert.ToInt32(s) - 32));
+                else y += s;
+            }
+            return y;
+        }
+
+        public static string Lowercase(string x)
+        {
+            string y = "";
+            x.ToCharArray();
+            foreach (char s in x)
+            {
+                // Checks if already lowercase
+                if (Convert.ToInt32(s) + 32 >= 97 && Convert.ToInt32(s) + 32 <= 122)
+                    y += Convert.ToChar((Convert.ToInt32(s) + 32));
+                else y += s;
+            }
+            return y;
+        }
+    }
+}
