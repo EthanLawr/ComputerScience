9e3c4765f03c8e989c8e6ee1ac1787f2cb87de6c
Ethan <28376972+Evildeathpro@users.noreply.github.com>
2019-03-07 11:38:26 -0500

    Add files via upload

---
+++ Computer Science/Projects Year 2/MultipleTopics_Program3_RollTheDie_Solution/MultipleTopics_Program3_RollTheDie/Form1.Designer.cs
@@ -0,0 +1,112 @@
+﻿namespace MultipleTopics_Program3_RollTheDie
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(232, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "How many sides are on the die you want to roll?";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 26);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(232, 20);
+            this.textBox1.TabIndex = 1;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 64);
+            this.label2.MaximumSize = new System.Drawing.Size(250, 150);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(0, 13);
+            this.label2.TabIndex = 2;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(195, 306);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(0, 13);
+            this.label3.TabIndex = 3;
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.25F);
+            this.button1.Location = new System.Drawing.Point(15, 277);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(142, 60);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Roll";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AcceptButton = this.button1;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Control;
+            this.ClientSize = new System.Drawing.Size(284, 349);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ Computer Science/Projects Year 2/MultipleTopics_Program3_RollTheDie_Solution/MultipleTopics_Program3_RollTheDie/Form1.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MultipleTopics_Program3_RollTheDie
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            try
+            {
+                // Make sure numbers dont go over the limit of the label's max size and is a correct number for arrays
+                if (int.Parse(textBox1.Text) > 157 || int.Parse(textBox1.Text) < 1) int.Parse("Error");
+                int number = int.Parse(textBox1.Text);
+                int[] myArray = new int[number];
+                Fill(myArray, number);
+                label2.Text = string.Join(" ", myArray);
+            } catch (Exception f)
+            {
+                // Error catching
+                MessageBox.Show("Please enter a valid number", "Alert!", MessageBoxButtons.OK);
+                label2.Text = "Input string was not in the right format. Please use an integer under 158 and above 0!";
+                textBox1.Text = "0";
+            }
+        }
+        public void Fill(int[] originalArray, int with)
+        {
+            // Makes an array full of the numbers needed
+            for (int i = 0; i < with; i++) originalArray[i] = i+1;
+        }
+        public Random randomGen = new Random();
+        public int Roll(int[] originalArray)
+        {
+            return originalArray[randomGen.Next(originalArray.Length)];
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                // Uses one number from the array and converts the array to an integer array
+                string[] newArray = label2.Text.Split(' ');
+                int[] intArray = new int[newArray.Length];
+                for (int i = 0; i < newArray.Length; i++) intArray[i] = int.Parse(newArray[i]);
+                label3.Text = Roll(intArray).ToString();
+            } catch (Exception f)
+            {
+                // Error catching
+                textBox1.Text = "0";
+                MessageBox.Show("Please enter a valid number", "Alert!", MessageBoxButtons.OK);
+                label2.Text = "Input string was not in the right format. Please use an integer under 158 and above 0!";
+            }
+
+        }
+    }
+}
