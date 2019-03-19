b857ca6498bab04af70f4e77ca2e58eaf7008687
Ethan <28376972+Evildeathpro@users.noreply.github.com>
2019-03-19 08:38:41 -0400

    Add files via upload

--- Computer Science/Projects AP/Chapter 11/Chapter 11 Project 3/src/SCRABBLE.java
+++ Computer Science/Projects AP/Chapter 11/Chapter 11 Project 3/src/SCRABBLE.java
@@ -9,16 +9,13 @@ public class SCRABBLE {
 	public static void main(String[] args) {
 		int score = 0;
         int[] EnglishScoreTable = { 1, 3, 3, 2, 1, 4, 2, 4, 1, 8, 5, 1, 3, 1, 1, 3, 10, 1, 1, 1, 1, 4, 4, 8, 4, 10 };
+        String English = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 
 		System.out.println("This program will ask a user to enter a SCRABBLE word and then it will determine its point value");
 		String Word = userInputMethod("Please enter a word: ");
 				
-        String English = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-
-        for (String Letter : Word.split(""))
-        {
-        	score += EnglishScoreTable[English.indexOf(Letter)];
-        }
+        for (String Letter : Word.split("")) score += EnglishScoreTable[English.indexOf(Letter)];
+        
 		System.out.print("The SCRABBLE value of this word is: " + score);
 	}
 	
