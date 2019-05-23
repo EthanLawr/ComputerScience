f0bead6cde4222f38335c75d7a8502010913ae7d
Ethan <28376972+Evildeathpro@users.noreply.github.com>
2019-05-23 08:35:01 -0400

    Add files via upload

---
+++ Computer Science/Projects AP/Final_Project/src/Location.java
@@ -0,0 +1,9 @@
+public class Location 
+{
+	// Member variables
+	public String roomTitle;
+	public String roomDescription;
+	public String directions;
+	public Location() {
+	}
+}
--- Computer Science/Projects AP/Final_Project/src/TextAdventure.java
+++ Computer Science/Projects AP/Final_Project/src/TextAdventure.java
@@ -1,3 +1,4 @@
+import java.io.FileNotFoundException;
 import java.util.Scanner;
 
 public class TextAdventure {
@@ -5,17 +6,27 @@ public class TextAdventure {
 	// This program will ask a user to enter a SCRABBLE word and then it will determine its point value
 	// 3/19/2019
 	// elawrence152@gmail.com
+	public static Location PlayerPosition = new Location();
+	public static Movement PlayerMovement = new Movement();
 	public static String input = "";
 	public static void main(String[] args) {
-		userInputMethod("Please enter something to do.");
-		InvalidOption(input);
+		try {
+			StoryFileReader StoryReader = new StoryFileReader(1, PlayerPosition);
+			System.out.print(PlayerPosition.roomDescription);
+			userInputMethod("Please enter something to do.");
+			if (input.equals("help")) {
+				System.out.print("Commands:\nhelp");
+			} else InvalidOption(input);
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		}
+		
 	}
-	
 	 // Method for a Nasty Gram in a user input
     public static String userInputMethod(String x) {
         try {
             Scanner userInput = new Scanner(System.in);
-            System.out.print(x);
+            System.out.print("\n" + x);
             String string = userInput.nextLine();
             if (string.length() == 0) return userInputMethod("You must enter a string with atleast one character!");
             input = string.toLowerCase();
