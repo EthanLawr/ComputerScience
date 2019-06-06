8f17ef47cdcd5659f7bb19c685e475e2f755d154
Ethan <28376972+Evildeathpro@users.noreply.github.com>
2019-06-06 11:08:56 -0400

    Update Location.java

--- Computer Science/Projects AP/Final_Project/src/Location.java
+++ Computer Science/Projects AP/Final_Project/src/Location.java
@@ -1,21 +1,59 @@
-
-public class Location
-{
+public class Location {
 	// Member variables
 	public String roomTitle, roomDescription, directions;
 	public static String[] directionsArray;
-	public static final String[] DirectionName = 
-		{"north","south","east","west","up","down","northeast","northwest","southeast","southwest"},
-		shortDirName = {"n","s","e","w","u","d","ne","nw","se","sw"},
-		allDirectionNames = {"north","south","east","west","up","down",
-				"northeast","northwest","southeast","southwest","n","s","e","w","u","d","ne","nw","se","sw"};
+	public static final String[] DirectionName = {
+			"north",
+			"south",
+			"east",
+			"west",
+			"up",
+			"down",
+			"northeast",
+			"northwest",
+			"southeast",
+			"southwest"
+		},
+		shortDirName = {
+			"n",
+			"s",
+			"e",
+			"w",
+			"u",
+			"d",
+			"ne",
+			"nw",
+			"se",
+			"sw"
+		},
+		allDirectionNames = {
+			"north",
+			"south",
+			"east",
+			"west",
+			"up",
+			"down",
+			"northeast",
+			"northwest",
+			"southeast",
+			"southwest",
+			"n",
+			"s",
+			"e",
+			"w",
+			"u",
+			"d",
+			"ne",
+			"nw",
+			"se",
+			"sw"
+		};
 	public static int stateOfRoom = 0;
-	public Location() {
-	}
+	public Location() {}
 	public void DirectionDisplay() {
 		System.out.print("\nYou can move in the following directions: ");
 		directionsArray = directions.split(",");
-	    for (String s : directionsArray) System.out.print(s + " ");
+		for (String s: directionsArray) System.out.print(s + " ");
 	}
 	public static boolean MovementCheck(String x, String DirectionCheckingFor) {
 		if (DirectionCheckingFor.equals("north") && (x.trim().equals("north") || x.trim().equals("n"))) return true;
@@ -30,5 +68,5 @@ public class Location
 		else if (DirectionCheckingFor.equals("southwest") && (x.trim().equals("southwest") || x.trim().equals("sw"))) return true;
 		else return false;
 	}
-	
+
 }
